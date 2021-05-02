0f1560758d248ea9cbebcdaf10029257f53f347d
Hammad-Rahman <hammad.rahman@chicagobooth.edu>
2021-05-02 05:18:40 +0000

    finished actors index

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
 
+  # Directors
   get("/directors", { :controller => "directors", :action => "index"})
-  
   get("/directors/eldest", { :controller => "directors", :action => "director_eldest"})
   get("directors/youngest", { :controller => "directors", :action => "director_youngest"})
-
   get("/directors/:director_id", { :controller => "directors", :action => "director_detail"})
 
 
   # Movies
-
-  
   get("/movies", { :controller => "movies", :action => "index"})
   get("/movies/:film_id", { :controller => "movies", :action => "details"})
 
+  # Actors
+  get("/actors", { :controller => "actors", :action =>"index"})
 end
